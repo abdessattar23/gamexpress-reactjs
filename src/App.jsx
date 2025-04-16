14a7aae8e32deaac01b361d78e399e44fffaa08d
abdellatif guezadi <abdellatifguezadi@gmail.com>
2025-04-16 15:56:28 +0100

    cartcontext

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import { AuthProvider } from "./context/AuthContext";
 import { ProductProvider } from "./context/ProductContext";
+import { CartProvider } from "./context/CartContext";
 import theme from "./theme/theme";
 import ProtectedRoute from "./components/ProtectedRoute";
 import AuthRoute from "./components/AuthRoute";
@@ -23,42 +24,44 @@ function App() {
       <Router>
         <AuthProvider>
           <ProductProvider>
-            <Routes>
-              <Route path="/" element={<Layout />}>
-                <Route index element={<Home />} />
+            <CartProvider>
+              <Routes>
+                <Route path="/" element={<Layout />}>
+                  <Route index element={<Home />} />
 
-                <Route element={<AuthRoute />}>
-                  <Route path="login" element={<Login />} />
-                  <Route path="register" element={<Register />} />
+                  <Route element={<AuthRoute />}>
+                    <Route path="login" element={<Login />} />
+                    <Route path="register" element={<Register />} />
+                  </Route>
+
+                  <Route path="unauthorized" element={<Unauthorized />} />
+
+                  {/* Product detail route - public */}
+                  <Route path="products/:id" element={<ProductDetail />} />
+
+                  {/* Protected routes */}
+                  <Route
+                    element={
+                      <ProtectedRoute roles={["product_manager", "super_admin"]} />
+                    }
+                  >
+                    <Route path="dashboard" element={<Dashboard />} />
+                  </Route>
+
+                  <Route element={<ProtectedRoute roles={["super_admin"]} />}>
+                    <Route path="categories" element={<Category />} />
+                  </Route>
+
+                  <Route
+                    element={
+                      <ProtectedRoute roles={["product_manager", "super_admin"]} />
+                    }
+                  >
+                    <Route path="products" element={<Product />} />
+                  </Route>
                 </Route>
-
-                <Route path="unauthorized" element={<Unauthorized />} />
-
-                {/* Product detail route - public */}
-                <Route path="products/:id" element={<ProductDetail />} />
-
-                {/* Protected routes */}
-                <Route
-                  element={
-                    <ProtectedRoute roles={["product_manager", "super_admin"]} />
-                  }
-                >
-                  <Route path="dashboard" element={<Dashboard />} />
-                </Route>
-
-                <Route element={<ProtectedRoute roles={["super_admin"]} />}>
-                  <Route path="categories" element={<Category />} />
-                </Route>
-
-                <Route
-                  element={
-                    <ProtectedRoute roles={["product_manager", "super_admin"]} />
-                  }
-                >
-                  <Route path="products" element={<Product />} />
-                </Route>
-              </Route>
-            </Routes>
+              </Routes>
+            </CartProvider>
           </ProductProvider>
         </AuthProvider>
       </Router>
